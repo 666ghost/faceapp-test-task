b0a07e4220dbb730d9abe64ea530d164c6865e3a
GHOST666CODER <ardasher559@gmail.com>
2020-11-15 18:41:52 +0500

    migrations

---
+++ monolite/database/migrations/2020_11_15_083452_create_exchange_rates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExchangeRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->id();
+            $table->float("rate");
+            $table->foreignId("currency_id")->constrained("currencies");
+            $table->foreignId("history_id")->constrained("exchange_rates_history");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+}
